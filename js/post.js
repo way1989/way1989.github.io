9d71ad5329b27b163343b62270ecaa14120c1e1f
way <1320438999@qq.com>
2016-05-15 22:29:09 +0800

    异步加载评论

--- js/post.js
+++ js/post.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 
     $('pre').addClass('prettyprint linenums'); // Google code Hight need these class
     $.getScript('/js/prettify/prettify.js', function(){ prettyPrint(); });
-
+		 //***********************
+     //**评论的代码也删掉哦***
+     window.disqus_shortname = 'broncho'; // required: replace example with your forum shortname
+     $('#disqus_container .comment').on('click',function(){
+         $(this).html('加载中...');
+         var that = this;
+         $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+     });
+     //**评论的代码也删掉哦***
+     //***********************
     markExternalLink();
 
     (function() {
